dc1f8edeb71a6dabcafc3c50612a88862190a272
danu <danujagreru@gmail.com>
2022-05-25 12:43:05 +0530

    put comments to the FactoryConfiguration class

--- src/lk/ijse/hibernate/util/FactoryConfiguration.java
+++ src/lk/ijse/hibernate/util/FactoryConfiguration.java
@@ -9,18 +9,25 @@ import org.hibernate.cfg.Configuration;
     @author DanujaV
     @created 24/05/2022 - 10:46 AM
 */
+
+//this FactoryConfiguration class use Singleton design pattern
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private SessionFactory sessionFactory;
 
     private FactoryConfiguration() {
+        // configure() -> load and config Hibernate.cfg.xml file to SessionFactory
+        // addAnnotatedClass() -> define which Entity that gonna use to Persist
         Configuration configuration = new Configuration().configure().addAnnotatedClass(Customer.class);
+
+        // build a SessionFactory and assign it to sessionFactory reference
         sessionFactory = configuration.buildSessionFactory();
     }
     public static FactoryConfiguration getInstance() {
         return (factoryConfiguration == null) ? factoryConfiguration = new FactoryConfiguration()
                 : factoryConfiguration;
     }
+    // return a new Session through sessionFactory
     public Session getSession() {
         return sessionFactory.openSession();
     }
